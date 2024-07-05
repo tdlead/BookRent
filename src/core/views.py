214613b3ff821009c0d147b5f9a8ea7c6daaeeed
Tatiana <dubineanschi.tatiana@gmail.com>
2024-07-05 11:36:49 +0200

    -dark mode

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from customers.models import Customer
 from books.models import Book, BookTitle
+from django.http import HttpResponseRedirect
+
+def change_theme(request):
+    if 'is_dark_mode' in request.session:
+        request.session['is_dark_mode'] = not request.session['is_dark_mode']
+    else:
+        request.session['is_dark_mode'] = True
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 def home_view(request):
     qs = Customer.objects.all()
